7a515f2acb65f4f112fe6b8d0cc2ca7a9bc5d165
ADRIAN FERNANDEZ FERNANDEZ <adrian.fernandez.fernandez@est.una.ac.cr>
2025-05-25 21:33:10 -0600

    Eliminacion Deducciones CCSS .H

--- proyecto c++/CCSS_LPT.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Deduccion.h"
-class CCSS_LPT :
-    public Deduccion
-{public:
-    virtual double calcular(double salarioBruto, double) const override;
-    virtual string obtenerNombre() const override;
-};
-
