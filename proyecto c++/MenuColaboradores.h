a43a0e160a05f40ef6b7ca00186099a0bc2d55ff
Erick Zamora C <erickzc2006@gmail.com>
2025-05-25 22:00:31 -0600

    Cambios

---
+++ proyecto c++/MenuColaboradores.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Consola.h"
+#include "Control.h"
+class MenuColaboradores : public consola
+{
+	private:
+		Control* gestor;
+	public:
+		MenuColaboradores(Control*);
+		virtual void lanzar(int) override;
+	};
+};
+
